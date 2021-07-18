7f9c8dc1858d88f0a2a5cb9adeb836b992e0b4aa
alex <alexrhunter@btinternet.com>
2021-07-18 14:31:51 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,44 @@
+function quicksort(sequence) {
+  if (sequence.length === 1) return sequence;
+
+  const pivot = sequence[sequence.length - 1];
+  const leftArray = [];
+  const rightArray = [];
+
+  for (let i = 0; i < sequence.length - 1; i++) {
+    sequence[i] < pivot
+      ? leftArray.push(sequence[i])
+      : rightArray.push(sequence[i]);
+  }
+
+  if (leftArray.length > 0 && rightArray.length > 0) {
+    return [...sortByField(leftArray), pivot, ...sortByField(rightArray)];
+  } else if (leftArray.length > 0) {
+    return [...sortByField(leftArray), pivot];
+  } else {
+    return [pivot, ...sortByField(rightArray)];
+  }
+}
+
+// quicksort to sort any sequence by a specific field
+function sortByField(sequence, field) {
+  if (sequence.length === 1) return sequence;
+
+  const pivot = sequence[sequence.length - 1];
+  const leftArray = [];
+  const rightArray = [];
+
+  for (let i = 0; i < sequence.length - 1; i++) {
+    sequence[i][field] < pivot[field]
+      ? leftArray.push(sequence[i])
+      : rightArray.push(sequence[i]);
+  }
+
+  if (leftArray.length > 0 && rightArray.length > 0) {
+    return [...sortByField(leftArray, field), pivot, ...sortByField(rightArray, field)];
+  } else if (leftArray.length > 0) {
+    return [...sortByField(leftArray, field), pivot];
+  } else {
+    return [pivot, ...sortByField(rightArray, field)];
+  }
+}
